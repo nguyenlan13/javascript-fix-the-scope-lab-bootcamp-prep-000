1dbc5d6e0d9afccb034c720a53444d36ffa50fbf
Lan Nguyen <nguyenlan13@gmail.com>
2019-02-06 07:42:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return animal
+  function doesnothardcode
 }
 
 function add2(n) {
